dd0c4086f0b963b8ec3f0ad81f409a4bec415abe
Albert Hovakimyan <HovakimyanAB@gmail.com>
2022-01-08 22:46:44 +0400

    add 1

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-D =
-pan_1 =
-tilt_1 =
-pan_2 =
-tilt_2 =
+import math
+
+D = 1500
+alpha1 = 30
+betta1 = 30
+alpha2 = 80
+betta2 = 85
+# AB / math.sin(alpha2) = BC / math.sin(alpha1) = D / math.sin(180-alpha1-alpha2)
+AF = math.sin(alpha2) * (D / math.sin(180-(alpha1+alpha2)))
+CF = math.sin(alpha1) * (D / math.sin(180-alpha1-alpha2))
+if AF / math.sin(alpha2) == CF / math.sin(alpha1) == D / math.sin(180-alpha1-alpha2):
+    print(AF, CF)
+
+AB = AF*math.tan(betta1)
+CB = CF*math.tan(betta2)
+
+H1 = math.sqrt(AB**2 - AF**2)
+H2 = math.sqrt(abs(CB**2) - abs(CF**2))
+print(H1,H2)
