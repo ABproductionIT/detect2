3586a543b226f0f92a0feb28aca6d73539ee5664
Albert Hovakimyan <HovakimyanAB@gmail.com>
2022-01-10 00:29:18 +0400

    changes

--- main.py
+++ main.py
@@ -2,23 +2,27 @@ import math
 import numpy as np
 
 
-D = 1500
+D = 100
 # D is a distance between cameras
+
+# test1
+# alpha1 = math.radians(60)
+# betta1 = math.radians(30)
+# alpha2 = math.radians(50)
+# betta2 = math.radians(27.053198)
+
+# test2
 alpha1 = math.radians(60)
-obrat = math.degrees(alpha1)
-print(alpha1, obrat)
-betta1 = math.radians(30)
-alpha2 = math.radians(50)
-betta2 = math.radians(30)
+betta1 = math.radians(89.9)
+alpha2 = math.radians(2)
+betta2 = math.radians(87)
+
 alpha3 = math.radians(180)-alpha1-alpha2
-alpha4 = math.radians(70)
-print(alpha3, alpha4)
-# 1.221730476396031 1.2217304763960306
 
 # AF / math.sin(alpha2) = CF / math.sin(alpha1) = D / math.sin(180-alpha1-alpha2)
 AF = (math.sin(alpha2) * D) / math.sin(alpha3)
 CF = (math.sin(alpha1) * D) / math.sin(alpha3)
-print("Distance from 1 =", str(AF)+"m", "Distance from 2 =", str(CF)+"m")
+print("map distance from 1 =", str(AF)+"m", "map distance from 2 =", str(CF)+"m")
 AB = AF / math.cos(betta1)
 CB = CF / math.cos(betta2)
 H1 = math.sin(betta1) * AB
